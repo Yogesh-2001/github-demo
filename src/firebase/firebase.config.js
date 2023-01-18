ae6e7a4d345a312c140c5021f248f9df385f1a85
Yogesh-2001 <yogeshyewale2001@gmail.com>
2023-01-18 18:52:11 +0530

    2nd commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAAqc3jngw0QmUpHzXffDpkrI0SheM97mU",
+  authDomain: "library-system-398c6.firebaseapp.com",
+  projectId: "library-system-398c6",
+  storageBucket: "library-system-398c6.appspot.com",
+  messagingSenderId: "1010701721017",
+  appId: "1:1010701721017:web:17a6608a9bf735d3b8ea68",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
